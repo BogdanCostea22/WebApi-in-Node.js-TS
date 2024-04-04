1aed14ee547d55f0bb234c26cbbe3e00cd699b7e
Bogdan Costea <41893216+BogdanCostea22@users.noreply.github.com>
2024-04-04 15:09:44 +0300

    feat: refactoring routing

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,38 @@ import { createServer, IncomingMessage, ServerResponse } from "http";
 import { PageNotFound } from "./errors/error";
 import { globalExceptionHandler } from "./errors/global-exception-handler";
 import { walletsController } from "./di";
+import {
+  WALLET_CREDIT_OPERATION,
+  WALLET_DEBIT_OPERATION,
+} from "./wallets/controllers/wallets-controller";
 
 const server = createServer(async (req, res) => {
-  globalExceptionHandler(definePaths, req, res);
-});
-
-async function definePaths(
-  req: IncomingMessage,
-  res: ServerResponse<IncomingMessage>
-): Promise<any> {
   const parsedUrl: UrlWithParsedQuery = parseUrl(req.url, true);
   const path: string = parsedUrl.pathname;
+
   const pathSegments: string[] = path
     .split("/")
-    .filter((value: string) => value.length > 0); // Split the path by '/'
+    .filter((value: string) => value.length > 0);
+  const walletID = pathSegments[1];
+  const walletOperation = pathSegments[2];
 
-  switch (pathSegments[0]) {
-    case walletsController.basePath: {
-      await walletsController.handleRequest(pathSegments, req, res);
-      break;
-    }
-    default: {
-      throw new PageNotFound();
-    }
+  if (pathSegments[0] !== walletsController.basePath) {
+    throw new PageNotFound();
   }
 
-  return;
-}
+  if (walletID && req.method === 'GET') {
+    //Get Wallet
+  }
+  if (walletOperation === WALLET_CREDIT_OPERATION && req.method === "POST") {
+    // Credit
+  }
+
+  if (walletOperation === WALLET_DEBIT_OPERATION && req.method === "POST") {
+    // Debit
+  }
+
+  throw new PageNotFound();
+});
 
 // starts a simple http server locally on port 3000
 server.listen(8080, "127.0.0.1", () => {
